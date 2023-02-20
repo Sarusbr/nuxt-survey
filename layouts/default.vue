76c828c71b60480ea232b420b42b9578771b239c
Sarusbr <batuilter2002@gmail.com>
2023-02-20 14:20:11 +0300

    default layout change

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,17 @@
 <template>
     <v-app>
-        <nuxt></nuxt>
+        <v-app-bar app color="primary">
+            <v-app-bar-title class="white--text title">
+                <div>
+                    {{ $store.state.title }}
+                </div>
+            </v-app-bar-title>
+        </v-app-bar>
+        <v-main>
+            <v-container>
+                <nuxt></nuxt>
+            </v-container>
+        </v-main>
     </v-app>
 </template>
 
